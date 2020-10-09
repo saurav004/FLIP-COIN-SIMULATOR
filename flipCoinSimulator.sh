50e084b9677bf08b6b0fe68540b5002f02b35a65
saurav004 <logtosaurav@gmail.com>
2020-10-10 01:00:13 +0530

    HEADS OR TAILS 10 TIMES

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,13 +5,20 @@ echo "---------------------------------------------------------------------Welco
 #HEADS AND TAILS ARE GIVEN VALUES 1 AND 0 REPECTIVELY
 HEADS=1
 TAILS=0
-coinSaid=$((RANDOM%2))
-
+HEADS_WIN_COUNTER=0
+TAILS_WIN_COUNTER=0
 #CHECKING IF HEAD WON OR TAIL WON 
 
+for((i=0;i<10;i++))
+do
+coinSaid=$((RANDOM%2))
 if [ $coinSaid == $HEADS ]
 then
+((HEADS_WIN_COUNTER++))
 echo "HEADS WON!!!!!!"
 else
+((TAILS_WIN_COUNTER++))
 echo "TAILS WON!!!!!!"
 fi
+done
+echo "HEADS WON $HEADS_WIN_COUNTER TIMES AND TAILS WON $TAILS_WIN_COUNTER TIMES"
