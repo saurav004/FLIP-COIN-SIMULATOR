f334042fbd16946a69dbddb3787fa8e10eaf4611
saurav004 <logtosaurav@gmail.com>
2020-10-10 00:50:07 +0530

    ADDED:check head won or tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,17 @@
 #! /bin/bash -x
 
 echo "---------------------------------------------------------------------Welcome To FILP Coin Simulator------------------------------------------------------------------------------------"
+
+#HEADS AND TAILS ARE GIVEN VALUES 1 AND 0 REPECTIVELY
+HEADS=1
+TAILS=0
+coinSaid=$((RANDOM%2))
+
+#CHECKING IF HEAD WON OR TAIL WON 
+
+if [ $coinSaid == $HEADS ]
+then
+echo "HEADS WON!!!!!!"
+else
+echo "TAILS WON!!!!!!"
+fi
